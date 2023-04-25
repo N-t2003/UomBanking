44b39e0a797baa23fb1e251a8150928d54566ef1
Dimitris Sparagis <96797730+dimsparagis0210@users.noreply.github.com>
2023-04-25 23:10:50 +0300

    Add files via upload

---
+++ src/main/java/org.example/User.java
@@ -0,0 +1,48 @@
+package org.example;
+
+/**
+ * By the software team of UOMSystemX
+ *
+ */
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "USERS")
+public class User {
+    @Id
+    @Column(name = "ID", nullable = false, length = 11)
+    private int id;
+
+    @Column(name = "EMAIL", nullable = false, length = 50)
+    private String email;
+
+
+//  getters and setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+//  toString method to fetch data from database
+
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
