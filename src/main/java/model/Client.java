e84c2892fd754e954ca65f701ccea433abb8b621
Dimitris Sparagis <96797730+dimsparagis0210@users.noreply.github.com>
2023-05-06 23:23:49 +0300

    Update Client.java

--- src/main/java/model/Client.java
+++ src/main/java/model/Client.java
@@ -3,6 +3,7 @@ package model;
 public class Client {
     public static void main(String[] main){
         System.out.println("HiHIHIHIHIHIH");
+        System.out.println("PSIFIZOUME STERGIOU");
 
     }
 }
