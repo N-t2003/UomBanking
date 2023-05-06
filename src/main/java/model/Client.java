579f1962d3bfbf1bb869aee7f8a78000ce12393c
LAPTOP-9AJL4JSN\Γιάννης Σφυράκης <ics22007@uom.edu.gr>
2023-05-07 02:11:43 +0300

    Account.java and Client.java was created!

--- src/main/java/model/Client.java
+++ src/main/java/model/Client.java
@@ -1,8 +1,25 @@
-package model;
 
 public class Client {
-    public static void main(String[] main){
-        System.out.println("HiHIHIHIHIHIH");
 
+    private String firstName, lastName, email, address, username, password;
+    private int phoneNumber;
+
+
+    public Client(String firstName, String lastName, int phoneNumber, String email,
+                  String address, String username, String password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+        this.address = address;
+        this.username = username;
+        this.password = password;
     }
+
+    public void printInfo(){
+
+     //soon
+    }
+
+    //Δεν χρειαζόμαστε Getters and Setters επειδή θα επικοινωνούμε απευθείας με την Βάση
 }
