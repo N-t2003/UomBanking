67ec33d2f0e08422ec2f24b4200d9b56cb75d08c
LAPTOP-9AJL4JSN\Γιάννης Σφυράκης <ics22007@uom.edu.gr>
2023-05-07 15:15:47 +0300

    eisai trompas

--- src/main/java/model/Main.java
+++ src/main/java/model/Main.java
@@ -17,7 +17,7 @@ public class Main {
     public static void main(String[] args) {
 
        /* try {
-            // Create aa new Document object
+            // Create aaa new Document object
             Document document = new Document();
             // Set the output file name and location
             PdfWriter.getInstance(document, new FileOutputStream("C:/Users/giann/Desktop/OSparEinaiArxidi.pdf"));
