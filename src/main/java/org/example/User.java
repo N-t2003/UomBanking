ea072e38ab736018056129002184a06d9e927bdd
LAPTOP-9AJL4JSN\Γιάννης Σφυράκης <ics22007@uom.edu.gr>
2023-05-10 00:07:08 +0300

    Test

--- src/main/java/org/example/User.java
+++ src/main/java/org/example/User.java
@@ -8,41 +8,102 @@ package org.example;
 import javax.persistence.*;
 
 @Entity
-@Table(name = "USERS")
+@Table(name = "Client")
 public class User {
-    @Id
-    @Column(name = "ID", nullable = false, length = 11)
-    private int id;
+    @Column(name = "FirstName", nullable = false, length = 50)
+    private String firstName;
 
-    @Column(name = "EMAIL", nullable = false, length = 50)
+    @Column(name = "LastName", nullable = false, length = 50)
+    private String lastName;
+
+    @Column(name = "PhoneNumber", nullable = false, length = 10)
+    private String phoneNumber;
+
+    @Column(name = "Email", nullable = false, length = 50)
     private String email;
+
+    @Column(name = "Address", nullable = false, length = 50)
+    private String address;
+
+    @Id
+    @Column(name = "Username", nullable = false, length = 50)
+    private String username;
+
+    @Column(name = "Password", nullable = false, length = 50)
+    private String password;
 
 
     //  getters and setters
-    public int getId() {
-        return id;
+
+    public String getFirstName() {
+        return firstName;
     }
 
-    public void setId(int id) {
-        this.id = id;
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
     }
 
     public String getEmail() {
         return email;
     }
 
+    public String getAddress() {
+        return address;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
     public void setEmail(String email) {
         this.email = email;
     }
 
-//  toString method to fetch data from database
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
 
-    @Override
+    //  toString method to fetch data from database
+
+
+ /*   @Override
     public String toString() {
         return "User{" +
                 "id=" + id +
                 ", email='" + email + '\'' +
                 '}';
     }
+
+  */
 }
+
+
