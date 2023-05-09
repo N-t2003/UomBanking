0d7b228fed6a8a27054ed44e22f79eaf4503c874
UOMSystemX <dimsparagis@gmail.com>
2023-05-09 16:18:49 +0300

    Enhanced MainFrame.java

--- src/main/java/gui/MainTest.java
+++ src/main/java/gui/MainTest.java
@@ -6,7 +6,7 @@ public class MainTest {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(new Runnable() {
             public void run() {
-                new CreateAccount();
+                new MainFrame();
             }
         });
     }
