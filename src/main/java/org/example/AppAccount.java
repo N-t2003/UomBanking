1208cda32efc1fe6f0efa84802b430c721998ede
LAPTOP-9AJL4JSN\Γιάννης Σφυράκης <ics22007@uom.edu.gr>
2023-05-16 18:37:47 +0300

    gei

--- src/main/java/org/example/AppAccount.java
+++ src/main/java/org/example/AppAccount.java
@@ -4,16 +4,8 @@ package org.example;
  * By the software team of UOMSystemX
  *
  */
-<<<<<<< HEAD:src/main/java/org/example/App.java
-import model.Withdraw;
-import org.hibernate.Hibernate;
-=======
+import org.hibernate.*;
 import model.Account;
-import model.Client;
->>>>>>> 6d9494a33348e15da5e870cdebcaeed37487a869:src/main/java/org/example/AppAccount.java
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
@@ -21,45 +13,42 @@ import org.hibernate.cfg.Configuration;
 public class AppAccount {
     public static void main(String[] args) {
 //      Setting up the transaction between the app and the database
-<<<<<<< HEAD:src/main/java/org/example/App.java
-        Configuration configuration = new Configuration().configure("hibernate.cfg.xml");
-=======
         Configuration configuration = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Account.class);
->>>>>>> 6d9494a33348e15da5e870cdebcaeed37487a869:src/main/java/org/example/AppAccount.java
         StandardServiceRegistry reg = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties()).build();
         SessionFactory sessionFactory = configuration.buildSessionFactory();
         Session session = sessionFactory.openSession(); //using the session to fetch data from the database(line 31)
 
         Transaction tx = session.beginTransaction();
 
-//      Creating a new user class
-<<<<<<< HEAD:src/main/java/org/example/App.java
-       // User user2 = new User();
-        User user1 = new User();
-        Withdraw withdraw = new Withdraw("", "22", 22, "22");
-=======
-//        User user2 = new User();
-//        User user1 = new User();
-        Account acc1 = new Account(100.2,"","","Astakos");
 
->>>>>>> 6d9494a33348e15da5e870cdebcaeed37487a869:src/main/java/org/example/AppAccount.java
+
+      //  Account acc1 = new Account(100.2,"","","sleeeeeet");
+
 
 //      Fetching a user from the database
-//        user1 = (User) session.get(User.class, 23445);
-//        System.out.println(user1);
+//          Account acc2 = (Account) session.get(Account.class, "*zk4E+yJ");
+         // System.out.println(session.get(Account.class, "*zk4E+yJ"));
 
 //      Adding columns to the database
         //user2.setEmail("dimsparagis@icloud.com");
 
 //      Saving the user
-<<<<<<< HEAD:src/main/java/org/example/App.java
-        session.save(withdraw);
-=======
-        session.save(acc1);
->>>>>>> 6d9494a33348e15da5e870cdebcaeed37487a869:src/main/java/org/example/AppAccount.java
+
+        Account account = (Account) session.get(Account.class, "*zk4E+yJ");
+
+
+        if (account != null) {
+           // System.out.println("Account Number: " + account.getAccountNumber());
+            System.out.println("Account Balance: " + account.getBalance() + account.getIBAN());
+            // Print other account properties as needed
+        } else {
+            System.out.println("Account not found.");
+        }
+
+       // session.save(acc2);
         session.getTransaction().commit();
         session.close();
-
+        sessionFactory.close();
     }
 
     public static void setuser(){
