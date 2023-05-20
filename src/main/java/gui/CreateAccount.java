c007a6d64a70862e5c63e2eac2f2fe8f59a85d7f
UOMSystemX <dimsparagis@gmail.com>
2023-05-20 14:20:58 +0300

    Enhanced CreateAccount frame functionality

--- src/main/java/gui/CreateAccount.java
+++ src/main/java/gui/CreateAccount.java
@@ -7,23 +7,24 @@ import javax.swing.border.Border;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Arrays;
 
 public class CreateAccount extends JFrame {
+    public JFrame createAccount;
     private JLabel header;
     private JLabel subHeader;
-    private JPanel infoPanel;
+    private CreateAccountPanel infoPanel;
     private JButton createAccountButton;
     private JLabel loginLabel;
     private JButton loginButton;
 
     public CreateAccount() {
 //      Setting up the template
-        JFrame createAccount = new TemplateNotLoggedIn();
+        createAccount = new TemplateNotLoggedIn();
 
 //      Initializing components
         header = Utils.setHeader("Create an UoMBanking account");
         subHeader = new JLabel("Start now with our free services");
-        infoPanel = new JPanel();
         createAccountButton = new JButton("Create your account");
         loginLabel = new JLabel("Already have an account?");
         loginButton = new JButton("Log in");
@@ -43,8 +44,16 @@ public class CreateAccount extends JFrame {
         createAccountButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                createAccount.dispose();
-                new MainFrame();
+//              Checking data validity
+                String phoneNumber = infoPanel.getPhoneNumber().getText();
+                String email = infoPanel.getEmail().getText();
+                String password =  infoPanel.getPasswordField().getText();
+                String confirmPassword  = infoPanel.getPasswordField2().getText();
+
+                if(checkData(email, phoneNumber, password, confirmPassword)) {
+                    createAccount.dispose();
+                    new MainFrame();
+                }
             }
         });
 
@@ -57,10 +66,12 @@ public class CreateAccount extends JFrame {
         loginButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+//              Checking data
                 createAccount.dispose();
                 new WelcomePage();
             }
         });
+
 
 //      Adding components to the frame
         createAccount.add(header);
@@ -77,220 +88,73 @@ public class CreateAccount extends JFrame {
         createAccount.setVisible(true);
         createAccount.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
-}
 
-class CreateAccountPanel extends JPanel{
-    private JCheckBox showPassword;
-    private JPasswordField passwordField;
-    private JPasswordField passwordField2;
-    private JButton signInButton;
-    private JLabel message;
-    private JLabel checkLabel;
-    private JComboBox costPerTransaction;
-    public CreateAccountPanel() {
-        showPassword = new JCheckBox("Show Password");
-        passwordField = new JPasswordField(10);
-        passwordField2 = new JPasswordField(10);
-        signInButton = new JButton("Sign In");
-        message = new JLabel("Make sure to use a strong password");
-        checkLabel = new JLabel();
-
-        costPerTransaction = new JComboBox();
-
-//      Setting up the message label
-        message.setFont(new Font("Courier", Font.PLAIN, 10));
-
-//      Setting up showPassword button
-        passwordField.setEchoChar('•');
-        showPassword.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                if (showPassword.isSelected()) {
-                    passwordField.setEchoChar((char) 0);
-                } else {
-                    passwordField.setEchoChar('•');
+    public boolean checkPhoneNumber(String number) {
+        char[] array = number.toCharArray();
+        boolean flag = true;
+        if(array.length == 10) {
+            for (char c: array) {
+                if (!Character.isDigit(c)) {
+                    System.out.println(!Character.isDigit(c));
+                    flag = false;
                 }
-            }
-        });
-
-//      Setting up the costPerTransaction combo box
-        DefaultComboBoxModel costPerTransactionModel = new DefaultComboBoxModel();
-        costPerTransactionModel.addElement("100");
-        costPerTransactionModel.addElement("500");
-        costPerTransactionModel.addElement("Other");
-        costPerTransaction.setModel(costPerTransactionModel);
-        costPerTransaction.setSelectedIndex(0);
-        costPerTransaction.setEditable(true);
-
-//      Setting the confirm password validation label
-        checkLabel.setSize(new Dimension(20, 20));
-
-
-//      Setting up the confirm password field
-        passwordField2.setEchoChar('•');
-        if (passwordField2.getPassword().length != 0) {
-            if (passwordField2.getText().equals(passwordField.getText())) {
-                checkLabel.setIcon(Utils.setLabelIcon("src/main/java/images/Valid.png", checkLabel));
-                checkLabel.setSize(20,20);
-                checkLabel.setFont(new Font("Courier", Font.PLAIN, 10));
-                checkLabel.setVisible(true);
-            } else {
-                checkLabel.setIcon(Utils.setLabelIcon("src/main/java/images/Invalid.png", checkLabel));
-                checkLabel.setSize(20,20);                                                     
-                checkLabel.setFont(new Font("Courier", Font.PLAIN, 10));
-                checkLabel.setVisible(true);
+                break;
             }
         }
+        else{
+            flag = false;
+        }
 
-//      Setting up signInButton
-        signInButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                new MainFrame();
-            }
-        });
-
-//      Basic setup for the panel
-        setBounds(350, 200, 500, 500);
-        setBackground(Color.LIGHT_GRAY);
-        Border border = BorderFactory.createTitledBorder("Enter your information");
-        setBorder(border);
-        setSize(new Dimension(500, 450));
-        layoutComponents();
+        return flag;
     }
 
-    public LayoutManager layoutComponents() {
-        setLayout(new GridBagLayout());
+    public boolean checkEmail(String email) {
+        char[] array = email.toCharArray();
+        boolean flag = false;
+        int i = 0;
+        while (!flag && i < array.length){
+            if (array[i] == '@') {
+                flag = true;
+            }
+            i++;
+        }
+        return flag;
+    }
 
-        GridBagConstraints gc = new GridBagConstraints();
+    public boolean checkPasswordConfirmation(String text1, String text2) {
+        char[] array = text1.toCharArray();
+        char[] array2 = text2.toCharArray();
+        boolean flag = true;
+        if (array != null && array2 != null){
+            if (!Arrays.equals(array, array2)) {
+                flag = false;
+            }
+        }
+        else if (array == null && array2 == null) {
+            flag = false;
+        }
+        return flag;
+    }
 
-//      First row-First column
-        gc.gridy = 0;
-
-        gc.weightx = 1;
-        gc.weighty = 0.1;
-        gc.gridx = 0;
-        gc.anchor = GridBagConstraints.LINE_END;
-        gc.insets = new Insets(0, 0, 0, 5);
-        add(new JLabel("First Name"), gc);
-
-//      First row-Second column
-        gc.gridx = 1;
-        gc.anchor = GridBagConstraints.LINE_START;
-        gc.insets = new Insets(0, 0, 0, 0);
-        add(new JTextField(10), gc);
-
-//      Second row-First column
-        gc.gridy++;
-
-        gc.weightx = 1;
-        gc.weighty = 0.1;
-        gc.gridx = 0;
-        gc.anchor = GridBagConstraints.FIRST_LINE_END;
-        gc.insets = new Insets(0, 0, 0, 5);
-        add(new JLabel("Last Name"), gc);
-
-//      Second row-Second column
-        gc.gridx = 1;
-        gc.anchor = GridBagConstraints.FIRST_LINE_START;
-        gc.insets = new Insets(0, 0, 0, 0);
-        add(new JTextField(10), gc);
-
-//      Third row-First column
-        gc.gridy++;
-        gc.gridx = 0;
-        gc.weighty = 0.1;
-        gc.weightx = 1;
-        gc.anchor = GridBagConstraints.FIRST_LINE_END;
-        gc.insets = new Insets(0, 0, 0, 0);
-        add(new JLabel("Phone Number"), gc);
-
-//      Third row-Second column
-        gc.gridx++;
-        gc.anchor = GridBagConstraints.FIRST_LINE_START;
-        gc.insets = new Insets(0, 0, 0, 0);
-        add(new JTextField(10), gc);
-
-//      Fourth row-First column
-        gc.gridy++;
-        gc.gridx = 0;
-        gc.weighty = 0.1;
-        gc.weightx = 1;
-        gc.anchor = GridBagConstraints.FIRST_LINE_END;
-        gc.insets = new Insets(0, 0, 0, 0);
-        add(new JLabel("Email"), gc);
-
-//      Fourth row-Second column
-        gc.gridx++;
-        gc.anchor = GridBagConstraints.FIRST_LINE_START;
-        gc.insets = new Insets(0, 0, 0, 0);
-        add(new JTextField(10), gc);
-
-//      Fifth row-First column
-        gc.gridy++;
-        gc.gridx = 0;
-        gc.weighty = 0.1;
-        gc.weightx = 1;
-        gc.anchor = GridBagConstraints.FIRST_LINE_END;
-        gc.insets = new Insets(0, 0, 0, 0);
-        add(new JLabel("Password"), gc);
-
-//      Fifth row-Second column
-        gc.gridx++;
-        gc.anchor = GridBagConstraints.FIRST_LINE_START;
-        gc.insets = new Insets(0, 0, 0, 0);
-        add(passwordField, gc);
-
-//      Fifth row-Second column
-        gc.gridx++;
-        gc.anchor = GridBagConstraints.FIRST_LINE_START;
-        gc.insets = new Insets(0, 0, 0, 0);
-        add(showPassword, gc);
-
-//      Sixth row-First column
-//        gc.gridy++;
-//        gc.gridx++;
-//        gc.weighty = 0.1;
-//        gc.weightx = 1;
-//        gc.anchor = GridBagConstraints.LAST_LINE_END;
-//        gc.insets = new Insets(0, 0, 0, 0);
-//        add(message, gc);
-
-//      Seventh row-First column
-        gc.gridy++;
-        gc.gridx = 0;
-        gc.weighty = 0.1;
-        gc.weightx = 1;
-        gc.anchor = GridBagConstraints.FIRST_LINE_END;
-        gc.insets = new Insets(0, 0, 0, 0);
-        add(new JLabel("Confirm Password"), gc);
-
-//      Seventh row-Second column
-        gc.gridx++;
-        gc.anchor = GridBagConstraints.FIRST_LINE_START;
-        gc.insets = new Insets(0, 0, 0, 0);
-        add(passwordField2, gc);
-
-//      Seventh row-Third column
-        gc.gridx++;
-        gc.anchor = GridBagConstraints.FIRST_LINE_START;
-        gc.insets = new Insets(0, 0, 0, 0);
-        add(checkLabel, gc);
-
-//      Eighth row-First column
-        gc.gridy++;
-        gc.gridx = 0;
-        gc.anchor = GridBagConstraints.FIRST_LINE_END;
-        gc.insets = new Insets(0, 0, 0, 0);
-        add(new JLabel("Cost per transaction"), gc);
-
-//      Eighth row-Second column
-        gc.gridx++;
-        gc.anchor = GridBagConstraints.FIRST_LINE_START;
-        gc.insets = new Insets(0, 0, 0, 0);
-        add(costPerTransaction, gc);
-
-        return null;
+    public boolean checkData(String email, String phoneNumber, String pass1, String pass2) {
+        if (!checkEmail(email)) {
+            JOptionPane.showMessageDialog(null, "Please insert a valid email",
+                    "Error", JOptionPane.ERROR_MESSAGE);
+            return false;
+        }
+        else if (!checkPhoneNumber(phoneNumber)) {
+            JOptionPane.showMessageDialog(null, "Please insert a valid phone number",
+                    "Error", JOptionPane.ERROR_MESSAGE);
+            return false;
+        }
+        else if (!checkPasswordConfirmation(pass1, pass2)) {
+            JOptionPane.showMessageDialog(null, "Confirm password failed",
+                    "Error", JOptionPane.ERROR_MESSAGE);
+            return false;
+        }
+        return true;
     }
 }
+
+
 
