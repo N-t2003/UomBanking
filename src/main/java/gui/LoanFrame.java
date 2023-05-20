5847a05c0d9eb9b03fe3a47548300bf8b71ff251
Vagiakos <133211228+Vagiakos@users.noreply.github.com>
2023-05-20 15:04:42 +0300

    test

---
+++ src/main/java/gui/LoanFrame.java
@@ -0,0 +1,97 @@
+package gui;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class LoanFrame extends JFrame {
+
+    private JLabel header;
+
+    private JLabel message;
+
+    private JLabel message1;
+
+    private JButton NewLoan;
+
+    private JButton ReturnToTheMainPage;
+
+    private JTable LoanTable;
+
+
+
+    public LoanFrame(){
+
+        JFrame Loans = new Template();
+
+        header = Utils.setHeader("Take loans with our bank system!");
+        message = new JLabel("Manage and pay your loans fast and easy");
+        message1 = new JLabel("Your loans");
+        NewLoan = new JButton("New Loan");
+        ReturnToTheMainPage = new JButton("Return to the main page");
+
+
+        String[] LoansColumns = new String[]{"Amount", "Expiration Date"};
+        String[][] LoansInfo = new String[][]{{"1000$","1/6/23"},{"90$","3/3/23"},{"250$","9/10/23"}};
+
+        LoanTable = new JTable(LoansInfo,LoansColumns);
+        JScrollPane jp = new JScrollPane(LoanTable);
+        jp.setBorder(BorderFactory.createLineBorder(Color.BLACK,2));
+        LoanTable.setForeground(Color.BLACK);
+        LoanTable.setEnabled(false);
+
+
+        header.setBounds(320, 100, 800, 100);
+        header.setFont(new Font("Courier",Font.PLAIN,35));
+
+        message.setBounds(400,140,600,80);
+        message.setFont(new Font("Courier",Font.PLAIN,20));
+
+        message1.setBounds(520,300,500,60);
+        message1.setFont(new Font("Courier", Font.PLAIN,30));
+
+        ReturnToTheMainPage.setBounds(1000,710,180,30);
+
+        NewLoan.setBounds(500,580,150,30);
+
+        jp.setBounds(440,360,300,130);
+
+        ReturnToTheMainPage.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                new MainFrame();
+            }
+        });
+
+        NewLoan.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+
+
+
+
+
+
+
+        Loans.add(header);
+        Loans.add(message);
+        Loans.add(message1);
+        Loans.add(ReturnToTheMainPage);
+        Loans.add(NewLoan);
+        Loans.add(jp);
+        Loans.setVisible(true);
+        Loans.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Loans.getContentPane().setBackground(Color.LIGHT_GRAY);
+
+
+
+
+
+    }
+
+}
---
+++ src/main/java/gui/SpendCategoriesFrame.java
@@ -0,0 +1,76 @@
+package gui;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class SpendCategoriesFrame extends JFrame {
+
+    private JLabel header;
+
+    private JLabel message;
+
+    private JButton ReturnToTheMainPage;
+
+    private JTable SCTable;
+
+
+
+    public SpendCategoriesFrame(){
+
+        JFrame SpendCategories = new Template();
+
+        header = Utils.setHeader("Spend Categories");
+        message = new JLabel("See what you spend in each category");
+        ReturnToTheMainPage = new JButton("Return to the main page");
+
+        String[] Columns = new String[]{"Transaction", "Amount"};
+        String[][] SCInfo = new String[][]{{"Supermarket","190.80$"},{"Pharmacy","380.76$"},{"Transport","100$"}};
+
+        SCTable = new JTable(SCInfo,Columns);
+        JScrollPane jp = new JScrollPane(SCTable);
+        jp.setBorder(BorderFactory.createLineBorder(Color.BLACK,2));
+        SCTable.setForeground(Color.BLACK);
+        SCTable.setEnabled(false);
+
+
+        header.setBounds(440, 100, 800, 100);
+        header.setFont(new Font("Courier",Font.PLAIN,40));
+
+        message.setBounds(480,140,600,80);
+        message.setFont(new Font("Courier",Font.PLAIN,15));
+
+        ReturnToTheMainPage.setBounds(530,440,180,30);
+
+        ReturnToTheMainPage.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                new MainFrame();
+            }
+        });
+
+        jp.setBounds(440,280,350,130);
+
+
+
+
+
+
+
+
+        SpendCategories.add(header);
+        SpendCategories.add(message);
+        SpendCategories.add(ReturnToTheMainPage);
+        SpendCategories.add(jp);
+        SpendCategories.setVisible(true);
+        SpendCategories.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        SpendCategories.getContentPane().setBackground(Color.LIGHT_GRAY);
+
+
+
+
+
+    }
+
+}
