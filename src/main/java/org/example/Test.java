2fb95d02b217a00898c52fd7ebe34391ef2a748c
UOMSystemX <dimsparagis@gmail.com>
2023-05-29 16:20:25 +0300

    Added database transaction methods for the Transactions(Deposit, Withdraw, Transfer, Payment) class

--- src/main/java/org/example/Test.java
+++ src/main/java/org/example/Test.java
@@ -1,10 +1,14 @@
 package org.example;
 
 import model.Account;
+import model.Withdraw;
+import model.Transactions;
 
 public class Test {
     public static void main(String[] args) {
-        Account account = new Account(1110, "", "", "");
-        AccountDB.saveAccount(account);
+        Transactions transaction = new Withdraw("", "22", 22, "22", "22");
+        TransactionsDB.saveTransaction(transaction);
+        Transactions tr = TransactionsDB.fetchTransaction(transaction.getID());
+        System.out.println("ID: " + tr.getID() + " Date: " + tr.getDate() + " Amount: " + tr.getAmount() + " Username: " + tr.getClientusername() + " Description: " + tr.getDescription() + " Type: " + tr.getClass().getName());
     }
 }
